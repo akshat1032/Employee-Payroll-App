3d382f15d4f8670622d049876221bb2daf871d19
akshat1032 <akshat.30oct@gmail.com>
2020-11-17 23:20:20 +0530

    Modified : Name and date validation added to script modified

--- js/EmployeePayrollForm.js
+++ js/EmployeePayrollForm.js
@@ -8,41 +8,71 @@ salary.addEventListener('input', function () {
 const EmployeePayrollRef = require("./EmployeePayroll");
 save = () => {
     let name = document.getElementById("name").value;
-    let gender;
-    if (document.getElementById("male").checked == true) {
-        gender = document.getElementById("male").value;
-    }
-    else {
-        gender = document.getElementById("female").value;
-    }
-    let salary = document.getElementById("salary").value;
+    let isNameValid = checkName(name);
     let day = document.getElementById("day").value;
     let month = document.getElementById("month").value;
     let year = document.getElementById("year").value;
-    let departmentArray = new Array();
-    if (document.getElementById("hr").checked == true) departmentArray.push(document.getElementById("hr").value);
-    if (document.getElementById("sales").checked == true) departmentArray.push(document.getElementById("sales").value);
-    if (document.getElementById("finance").checked == true) departmentArray.push(document.getElementById("finance").value);
-    if (document.getElementById("engineer").checked == true) departmentArray.push(document.getElementById("engineer").value);
-    if (document.getElementById("others").checked == true) departmentArray.push(document.getElementById("others").value);
+    let isDateValid = dateCheck(day, month, year);
 
-    if (gender == "male") gender = "m";
-    else gender = "f";
+    if ((isNameValid) && (isDateValid)) {
+        let departmentArray = new Array();
+        if (document.getElementById("hr").checked == true) departmentArray.push(document.getElementById("hr").value);
+        if (document.getElementById("sales").checked == true) departmentArray.push(document.getElementById("sales").value);
+        if (document.getElementById("finance").checked == true) departmentArray.push(document.getElementById("finance").value);
+        if (document.getElementById("engineer").checked == true) departmentArray.push(document.getElementById("engineer").value);
+        if (document.getElementById("others").checked == true) departmentArray.push(document.getElementById("others").value);
 
-    if (month == "Jan") month = 1;
-    if (month == "Feb") month = 2;
-    if (month == "Mar") month = 3;
-    if (month == "Apr") month = 4;
-    if (month == "May") month = 5;
-    if (month == "June") month = 6;
-    if (month == "July") month = 7;
-    if (month == "Aug") month = 8;
-    if (month == "Sep") month = 9;
-    if (month == "Oct") month = 10;
-    if (month == "Nov") month = 11;
-    if (month == "Dec") month = 12;
+        let gender;
+        if (document.getElementById("male").checked == true) {
+            gender = document.getElementById("male").value;
+        }
+        else {
+            gender = document.getElementById("female").value;
+        }
+        let salary = document.getElementById("salary").value;
+        if (gender == "male") gender = "m";
+        else gender = "f";
 
-    let startDate = new Date(year, month, day);
-    let employeePayrollData = new EmployeePayrollRef.EmployeePayroll(name, salary, gender, startDate, departmentArray);
-    console.log(employeePayrollData.toString());
+        if (month == "Jan") month = 1;
+        if (month == "Feb") month = 2;
+        if (month == "Mar") month = 3;
+        if (month == "Apr") month = 4;
+        if (month == "May") month = 5;
+        if (month == "June") month = 6;
+        if (month == "July") month = 7;
+        if (month == "Aug") month = 8;
+        if (month == "Sep") month = 9;
+        if (month == "Oct") month = 10;
+        if (month == "Nov") month = 11;
+        if (month == "Dec") month = 12;
+
+        let startDate = new Date(year, month, day);
+        let employeePayrollData = new EmployeePayrollRef.EmployeePayroll(name, salary, gender, startDate, departmentArray);
+        console.log(employeePayrollData.toString());
+    }
+    else
+        console.log("Incorrect name or date!!")
+}
+
+checkName = (name) => {
+    let nameRegex = RegExp('^[A-Z]{1}[a-z]{2,}$');
+    if (!(nameRegex.test(name))) throw "Name is incorrect"
+    return true;
+}
+
+dateCheck = (day, month, year) => {
+    if (month == "Feb" && (year % 4 == 0 || year % 100 == 0 && year % 400 == 0)) {
+        if (day > 29) throw "Invalid date"
+        return true;
+    }
+
+    if (month == "Feb") {
+        if (day > 28) throw "Invalid Date"
+        return true;
+    }
+
+    if (month == "April" || month == "June" || month == "Sep" || month == "Nov") {
+        if (day > 30) throw "Invalid Date"
+        return true;
+    }
 }
