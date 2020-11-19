c39bec8426afdb2bb739f4db2e75fb87b682b12c
akshat1032 <akshat.30oct@gmail.com>
2020-11-19 16:27:08 +0530

    Added : Display employee details using template literals variant added

--- js/EmployeePayrollHome.js
+++ js/EmployeePayrollHome.js
@@ -3,23 +3,16 @@ window.addEventListener('DOMContentLoaded', (event) => {
 });
 
 const createInnerHTML = () => {
+    const headerHtml = "<th></th><th>Name</th><th>Gender</th><th>Department</th><th>Salary</th><th>Start Date</th><th>Actions</th>";
     const innerHtml =
-        `<tr>
-            <th></th>
-            <th>Name</th>
-            <th>Gender</th>
-            <th>Department</th>
-            <th>Salary</th>
-            <th>Start Date</th>
-            <th>Actions</th>
-        </tr>
+        ` ${headerHtml}
         <tr>
             <td><img class="profile" alt="" src="../assets/profile-images/Ellipse -3.png"></td>
             <td>Akshat Sambodhi</td>
             <td>Male</td>
             <td>
-            <div class="dept-label">Sales</div>
-            <div class="dept-label">Finance</div>
+                <div class="dept-label">Engineering</div>
+                <div class="dept-label">Sales</div>
             </td>
             <td>5000000</td>
             <td>4 Nov 2020</td>
@@ -29,5 +22,5 @@ const createInnerHTML = () => {
                     src="../assets/icons/create-black-18dp.svg">
             </td>
         </tr>`;
-    document.querySelector('#table-display').innerHTML=innerHtml;
+    document.querySelector('#table-display').innerHTML = innerHtml;
 }
